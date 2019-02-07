88817c01c3b07b06a7dba9cfdd5dd124da451c46
Jacob Hayes <jacob.r.hayes@gmail.com>
2019-02-07 15:06:56 -0600

    Only set tmux vars when inside tmux

--- dotfiles/.config/fish/conf.d/_tmuxpwd.fish
+++ dotfiles/.config/fish/conf.d/_tmuxpwd.fish
@@ -4,9 +4,13 @@ function _set_tmuxpwd --on-variable PWD --description "Set a pane specific tmux 
     end
     # tmux's pane_current_path doesn't work reliably with fish 3, so we'll set this var for each pane and reference that
     # in ~/.tmux.conf.
-    tmux setenv TMUXPWD_(tmux display -p "#D" | tr -d %) "$PWD"
+    if set -q TMUX
+        tmux setenv TMUXPWD_(tmux display -p "#D" | tr -d %) "$PWD"
+    end
 end
 
 function _unset_tmuxpwd --on-event fish_exit --description "Clean up the pane specific tmux PWD var on shell exit."
-    tmux setenv -u TMUXPWD_(tmux display -p "#D" | tr -d %)
+    if set -q TMUX
+        tmux setenv -u TMUXPWD_(tmux display -p "#D" | tr -d %)
+    end
 end
