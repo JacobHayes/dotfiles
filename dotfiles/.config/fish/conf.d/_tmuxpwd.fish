2175e49d2439f7b6918541220aeb76e8ec81129c
Jacob Hayes <jacob.r.hayes@gmail.com>
2019-02-13 14:20:13 -0600

    Use tmux global (not session scoped) vars for TMUXPWD. As long as I only use a single grouped session, should be fine.

--- dotfiles/.config/fish/conf.d/_tmuxpwd.fish
+++ dotfiles/.config/fish/conf.d/_tmuxpwd.fish
@@ -2,12 +2,12 @@ function _set_tmuxpwd --on-event fish_prompt --description "Set a pane specific 
     # tmux's pane_current_path doesn't work reliably with fish 3, so we'll set this var for each pane and reference that
     # in ~/.tmux.conf.
     if set -q TMUX
-        tmux setenv TMUXPWD_(tmux display -p "#D" | tr -d %) "$PWD"
+        tmux setenv -g TMUXPWD_(tmux display -p "#D" | tr -d %) "$PWD"
     end
 end
 
 function _unset_tmuxpwd --on-event fish_exit --description "Clean up the pane specific tmux PWD var on shell exit."
     if set -q TMUX
-        tmux setenv -u TMUXPWD_(tmux display -p "#D" | tr -d %)
+        tmux setenv -gu TMUXPWD_(tmux display -p "#D" | tr -d %)
     end
 end
