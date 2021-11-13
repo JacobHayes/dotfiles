85ea14c4e66096582517811d092c3115cb172196
Jacob Hayes <jacob.r.hayes@gmail.com>
2021-11-13 00:17:44 -0500

    Update Alfred settings

--- apps/alfred/setup.sh
+++ apps/alfred/setup.sh
@@ -6,5 +6,6 @@ set -o pipefail
 
 HERE="$(cd "$(dirname "$0")" ; pwd -P)"
 
-mkdir -p "${HOME}/Library/Application Support/Alfred 3"
-ln -fs "${HERE}/Alfred.alfredpreferences" "${HOME}/Library/Application Support/Alfred 3/"
+mkdir -p "${HOME}/Library/Application Support/Alfred/Alfred.alfredpreferences/"
+# Ignore the "remote" preferences
+ln -fs "${HERE}/preferences" "${HOME}/Library/Application Support/Alfred/Alfred.alfredpreferences/"
