fc468beacecaf28e5db5831127638f1a37f238da
Jacob Hayes <jacob.r.hayes@gmail.com>
2022-02-11 21:02:44 -0500

    Fix gclone dir

--- dotfiles/.config/fish/functions/gclone.fish
+++ dotfiles/.config/fish/functions/gclone.fish
@@ -2,5 +2,5 @@ function gclone
     # ex: pandas-dev/pandas
     mkdir -p ~/src/github.com/(dirname $argv)
     git clone git@github.com:$argv.git ~/src/github.com/$argv
-    cd ~/src/github.com/(dirname $argv)
+    cd ~/src/github.com/$argv
 end
